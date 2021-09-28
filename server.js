9bddc1fd13c1144af772643ae8eead70f8a2a4ea
eva <83175995+evapopp@users.noreply.github.com>
2021-09-28 09:22:40 -0700

    trouble shooting routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const notes = require('./db/db.json');
+const fs = require('fs');
 
 const PORT = 3001;
 
@@ -8,7 +9,6 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
 
 // get route for homepage
@@ -16,8 +16,28 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/api/notes', (req, res) => {res.json(notes)
+    console.info('this was a get req')
+});
 
-app.get('/api/notes', (req, res) => res.json(notes));
+app.post('/api/notes', (req, res) => {
+    res.json(`POST request received`);
+    res.json(req.body);
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text
+    }
+    notes.push(newNote);
+    fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(notes))
+    // notes.push(req.body);
+    // console.info(notes)
+    // fs.writeFile('./db/db.json', JSON.stringify(notes), (err) =>
+    //     err ? console.log(err) : console.log('success'));
+    
+    // console.info(`${req.method} request received`);
+});
+
+
 
 
 
