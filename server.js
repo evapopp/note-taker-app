2e839277b3575842b8cfd0a7d2c2a8b5159e1428
eva <83175995+evapopp@users.noreply.github.com>
2021-09-28 10:32:39 -0700

    added PORT, attempting heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const notes = require('./db/db.json');
 const fs = require('fs');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -14,31 +14,32 @@ app.use(express.static('public'));
 // get route for homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+    console.info('this worked!')
 });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+    console.info('this also worked!')
+});
+
 
 app.get('/api/notes', (req, res) => {res.json(notes)
     console.info('this was a get req')
 });
 
+
 app.post('/api/notes', (req, res) => {
-    res.json(`POST request received`);
-    res.json(req.body);
+    console.info('testing this!')
     const newNote = {
         title: req.body.title,
         text: req.body.text
     }
-    notes.push(newNote);
-    fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(notes))
-    // notes.push(req.body);
-    // console.info(notes)
-    // fs.writeFile('./db/db.json', JSON.stringify(notes), (err) =>
-    //     err ? console.log(err) : console.log('success'));
-    
-    // console.info(`${req.method} request received`);
+    // console.info('notes', notes)
+    // console.info('newnote', newNote)
+    notes.push(newNote)
+    res.json(notes);
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => err ? console.log(err) : console.log("we did it "));
 });
-
-
-
 
 
 app.listen(PORT, () => {
