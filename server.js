c7c077bed39bfa2a5fc8410629b004059f3baff2
eva <83175995+evapopp@users.noreply.github.com>
2021-10-11 17:18:38 -0700

    updated get routes to connect to pages correctly

--- server.js
+++ server.js
@@ -12,26 +12,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// GET * should return the index.html file.
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-    console.info('this worked!')
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-// GET /notes should return the notes.html file.
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/notes.html'));
-    console.info('this also worked!')
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-// GET /api/notes should read the db.json file and return all saved notes as JSON.
 app.get('/api/notes', (req, res) => {res.json(notes)
-    console.info('this was a get req')
 });
 
-// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 app.post('/api/notes', (req, res) => {
-    console.info('testing this!')
     const newNote = {
         id: uuid.v4(),
         title: req.body.title,
@@ -39,7 +31,7 @@ app.post('/api/notes', (req, res) => {
     };
     notes.push(newNote)
     res.json(notes);
-    fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => err ? console.log(err) : console.log("we did it "));
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => err ? console.log(err) : console.log("You made a new note!"));
 });
 
 
